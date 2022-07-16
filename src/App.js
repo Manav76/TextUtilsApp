05ec258437f97f89b0caa1ed32b525348ce71773
Manav <mjfx102@gmail.com>
2022-07-16 16:26:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, { useState } from "react";
+import Navbar from './components/Navbar';
+// import About from './components/About';
+import Textform from './components/textform';
+import Alert from './components/Alert';
 function App() {
+  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [alert , setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+
+    },2500 );
+  } 
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark Mode has been enabled" , "success");
+    
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+     showAlert("Light Mode has been enabled" , "success");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title = "TextUtils" mode = {mode} toggleMode = {toggleMode} />
+    <Alert  alert = {alert} />
+    <div className = "container my-3">
+      <Textform heading="Enter the text to analyze below"  />
     </div>
+  </>
   );
 }
+
 
 export default App;
---
+++ src/components/textform.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+export default function Textform(props) {
+  const handleUpClick = () => {
+    console.log("Uppercase was Clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleLoClick = () => {
+    console.log("Uppercase was Clicked");
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const removeExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+  }
+  const[text , setText] = useState('');
+  return (
+    <>
+      <div>
+        <h1>{props.heading} </h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="9"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={removeExtraSpaces}>
+          Remove Extra Space
+        </button>
+      </div>
+      <div className="container my-7">
+        <h2>Your text summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
